0eb6be2995cfbef6d4a150f9e6db705a3236cf65
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2025-02-02 21:43:27 -0600

    Consumiendo la API y pintando con grid

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,6 +1,6 @@
 import { FaCirclePlus } from "react-icons/fa6";
 
-const Card = () => {
+const Card = ({ data }) => {
 
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg shadow-md p-2 hover:shadow-xl transition-all duration-250'>
@@ -8,12 +8,12 @@ const Card = () => {
                 <span
                     className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-1 p-1'
                 >
-                    Electronics
+                    { data.category.name }
                 </span>
                 <img
                     className='w-full h-full object-cover rounded-lg'
-                    src='https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-                    alt='headphones'
+                    src={ data.images }
+                    alt={ data.title }
                 />
                 <button
                     className="absolute top-0 right-0 flex justify-center items-center rounded-full m-1.5 cursor-pointer transition-all duration-500 ease-in-out hover:rotate-180"
@@ -23,8 +23,8 @@ const Card = () => {
 
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light'> { data.title } </span>
+                <span className='text-lg font-medium'> ${ data.price } </span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
+// useEffect siempre nos ayuda al consumir una API
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from '../../Components/Card'
 
 const Home = () => {
 
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, [])
+
     return (
         <Layout>
             Home
-            <Card />
+            <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                {
+                    items?.map((item) => (
+                        <Card key={item.id} data={item} />
+                    ))
+                }
+            </div>
         </Layout>
     );
 }
