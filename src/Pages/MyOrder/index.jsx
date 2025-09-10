beb18cd08143d82dcef2eaeaddc209739abc8187
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2025-09-09 23:40:46 -0600

    Preparando MyOrders

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,6 +2,8 @@ import { Layout } from "../../Components/Layout";
 import { useContext } from "react";
 import { CheckoutSideMenuContext } from "../../Context";
 import { OrderCard } from "../../Components/OrderCard";
+import { IoChevronBackCircleOutline } from "react-icons/io5";   
+import { Link } from "react-router-dom";
 
 const MyOrder = () => {
 
@@ -11,7 +13,13 @@ const MyOrder = () => {
 
     return (
         <Layout>
-            My Order
+            <div className='flex gap-6'>
+                <Link to={'/my-orders'}>
+                    <IoChevronBackCircleOutline className='h-6 w-6 text-black cursor-pointer' />
+
+                </Link>
+                <h1>My Order</h1>
+            </div>
 
             {/* La lista de mi orden */}
             <div className='flex flex-col w-80 mt-6'>
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,10 +1,27 @@
+import { useContext } from "react";
 import { Layout } from "../../Components/Layout";
+import { OrdersCard } from "../../Components/OrdersCard";
+import { CheckoutSideMenuContext } from "../../Context";
+import { Link } from "react-router-dom";
 
 const MyOrders = () => {
+    const context = useContext(CheckoutSideMenuContext)
     
     return (
         <Layout>
             My Orders
+            {
+                context.order.map((order, index) => {
+
+                    <Link key={index} to={`/my-orders/${order.id}`}>
+                        <OrdersCard
+                            totalPrice={order.totalPrice}
+                            totalProducts={order.totalProducts}
+                        />
+                    </Link>
+
+                })
+            }
         </Layout>
     );
 }
