03665fbde0c9747082c4319d36c45d358445d726
FernandoLabastida <labastidafernando@comunidad.unam.mx>
2025-10-14 23:48:06 -0600

    Configuraciones necesarias para hacer el deploy en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/curso-react-vite-tailwindcss/',
   plugins: [
     react(),
     tailwindcss()
